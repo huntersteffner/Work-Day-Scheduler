4839f900832d9cb077d7f55746f4305dcea51da8
huntersteffner <huntersteffner@gmail.com>
2022-06-29 18:53:21 -0400

    Modified style and updated readme

--- Assets/script.js
+++ Assets/script.js
@@ -1,32 +1,15 @@
 'use strict'
 
-
-
-const dateEl = $('#currentDay')
-const am8 = $('#am8')
-const am8text = $('.am8text')
-const amOrPmEl = $('.amOrPmEl')
-
-const nine = $('#nine')
-
-
+// Time variables
 const date = moment().format('dddd: MMMM Do, YYYY')
 const time = moment().format('h:mm:ss a')
 const hourOfDay = parseInt(moment().hour())
+$('#currentDay').text(date + ' at ' + time)
 
-console.log()
-
-dateEl.text(date + ' at ' + time)
-
-
-console.log(moment().hour())
-
+// Function for list that changes hours with colors marked either past, present, or future.
 $('.hour-marker').each(function() {
     const curHour = parseInt($(this).attr('id'))
-    console.log(curHour)
     const curId = `#${curHour}text`
-    console.log(curId)
-
     if(hourOfDay < curHour) {
         $(curId).addClass('future')
     } else if (hourOfDay == curHour) {
@@ -36,29 +19,17 @@ $('.hour-marker').each(function() {
     }
 })
 
-// $('.save').each(function(e) {
-//     e.target.on('click', function() {
-//         const curSave = $(this).attr('id')
-//         console.log(curSave)
-
-//     })
-// })
-
-$('.save').on('click', function(e) {
-    const curSave = $(this).attr('id')
-
+// Save button to local storage
+$('.saveBtn').on('click', function() {
     const localKey = $(this).parent().attr('id')
     const localText = $(this).siblings('textarea').val()
-    
-    console.log(`${localKey}: ${localText}`)
     localStorage.setItem(localKey, localText)
-
+    // Send message indicating it successfully saved
     $(this).text('Saved')
 })
 
 $('.textbox').each(function() {
     const curKey = $(this).parent().attr('id')
-    // console.log(curKey)
     const savedText = localStorage.getItem(curKey)
     console.log(savedText)
     $(this).text(savedText)
